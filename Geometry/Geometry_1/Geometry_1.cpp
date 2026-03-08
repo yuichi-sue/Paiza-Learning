ef8c7e767d13042a3576a57064ed1e8e60428cf4
kouyus_ <265691968+yuichi-sue@users.noreply.github.com>
2026-03-09 07:41:29 +0900

    Update Geometry_1.cpp

--- Geometry/Geometry_1/Geometry_1.cpp
+++ Geometry/Geometry_1/Geometry_1.cpp
@@ -9,7 +9,7 @@ int main() {
     long long dy = y2 - y1;
     long long dsq = (dx * dx) + (dy * dy);
 
-    // 円と円の内外判定（接点含む）.
+    // 半径の合計以下なら、円は重なっている（接点を含む）.
     if (dsq <= (r1 + r2) * (r1 + r2)) {
         std::cout << "Yes\n";
     }
