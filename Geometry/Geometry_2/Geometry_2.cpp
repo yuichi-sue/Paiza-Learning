645abcfd67c863c21c004358100c4730f021aab2
kouyus_ <265691968+yuichi-sue@users.noreply.github.com>
2026-03-09 17:41:27 +0900

    Update Geometry_2.cpp
    関数分けし、境界軸判定を追加

--- Geometry/Geometry_2/Geometry_2.cpp
+++ Geometry/Geometry_2/Geometry_2.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+bool DetectCircleAABB(long long x1, long long y1, long long r, long long x2, long long y2, long long x3, long long y3) {
+	// 円に最も近い点を求める.
+	long long px, py;
+	px = std::min(std::max(x1, x2), x3);
+	py = std::min(std::max(y1, y2), y3);
+
+	// 点と円の内外判定.
+	long long dx, dy;
+	dx = px - x1;
+	dy = py - y1;
+	long long dsq = (dx * dx) + (dy * dy);
+	return dsq <= (r * r);
+}
+
 int main() {
 	long long x1, y1, r;		// 円の情報.
 	long long x2, y2, x3, y3;	// 軸並行長方形の情報.
@@ -18,17 +32,13 @@ int main() {
 		std::swap(y2, y3);
 	}
 
-	// 円に最も近い点を求める.
-	long long px, py;
-	px = std::min(std::max(x1, x2), x3);
-	py = std::min(std::max(y1, y2), y3);
+	// 接触していないことが明らかな場合、早期リターン
+	if (x1 + r < x2 || x1 - r > x3 || y1 + r < y2 || y1 - r > y3) {
+		std::cout << "No\n";
+		return 0;
+	}
 
-	// 点と円の内外判定.
-	long long dx, dy;
-	dx = px - x1;
-	dy = py - y1;
-	long long dsq = (dx * dx) + (dy * dy);
-	if (dsq <= (r * r)) {
+	if (DetectCircleAABB(x1, y1, r, x2, y2, x3, y3)) {
 		std::cout << "Yes\n";
 	}
 	else {
